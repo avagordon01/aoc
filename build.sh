d15a3f2340ec30e4ff9deb8375110380c9dffda5
Ava Gordon <avagordon01@gmail.com>
2025-12-29 16:02:39 +0000

    new laptop, remove parallelism limit

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -ex
 #Ninja Multi-Config allows quick & easy switching between Debug and Release builds without removing and rebuilding the whole build dir
 cmake -G "Ninja Multi-Config" -S . -B build
 config=Debug
-cmake --build build --config ${config} --parallel 1
+cmake --build build --config ${config}
 cmake --install build --config ${config} --prefix install
